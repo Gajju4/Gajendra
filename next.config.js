de0eba0dfc3ecec6ae46455006c6fd406e354eef
unknown <gajendraiitkgp@gmail.com>
2025-03-16 15:03:41 +0530

    Fix: Force update profile picture with cache busting

--- next.config.js
+++ next.config.js
@@ -8,9 +8,11 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    minimumCacheTTL: 0,
   },
   basePath: process.env.NODE_ENV === 'production' ? '/Gajendra' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Gajendra' : '',
+  generateEtags: false,
 }
 
 module.exports = nextConfig 
